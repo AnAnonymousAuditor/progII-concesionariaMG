dd758331f2bc446ffdd99b27a81107a09889faec
ManuelBotas <manuelbotas2001@gmail.com>
2026-05-06 09:41:14 -0300

    primer commit

---
+++ src/main/java/laboratorio2/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package laboratorio2;
+
+/**
+ *
+ * @author manuel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
