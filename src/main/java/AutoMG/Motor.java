c60aacc412954e5f63a32ce6bfbc08a7d2ceab3a
ManuelBotas <manuelbotas2001@gmail.com>
2026-05-06 09:54:52 -0300

    agregadas clases Auto y Motor

--- src/main/java/AutoMG/Motor.java
+++ src/main/java/AutoMG/Motor.java
@@ -1,16 +1,13 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
-package laboratorio2;
+package AutoMG;
 
 /**
  *
  * @author manuel
  */
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Motor {
+    
 }
